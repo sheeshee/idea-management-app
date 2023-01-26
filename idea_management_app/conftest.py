8f7b3f08eb58d891f2e220235491d67e0a2f57bf
Sam Sheehy <samuelsheehy95@gmail.com>
2023-01-26 15:25:27 +0000

    Initial commit (based of cookiecutter-django)

---
+++ idea_management_app/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from idea_management_app.users.models import User
+from idea_management_app.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ idea_management_app/users/tests/test_models.py
@@ -0,0 +1,5 @@
+from idea_management_app.users.models import User
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.username}/"
